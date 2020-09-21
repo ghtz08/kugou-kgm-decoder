d2c9d0ad02826a7aa4db91c8e9cc55ee21cc7e43
TanZhou <tanzhou08@qq.com>
2020-09-21 23:07:49 +0800

    feat: 补充剩余参数的解析（未实现对应逻辑）

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,12 @@
 use clap::{App, Arg, ArgMatches};
 use lazy_static::lazy_static;
 
+#[cfg_attr(debug_assertions, derive(Debug))]
 pub struct Config {
     pub target: String,
+    pub recursive: bool,
+    pub keep_file: bool,
+    pub stdout: bool,
 }
 
 pub fn get<'a>() -> &'a Config {
@@ -10,7 +14,16 @@ pub fn get<'a>() -> &'a Config {
         static ref CFG: Config = (|| {
             let args = parse_args();
             let target = args.value_of("target").unwrap().to_string();
-            Config { target }
+            let recursive = args.is_present("recursive");
+            let keep_file = args.is_present("keep");
+            let stdout = args.is_present("stdout");
+
+            Config {
+                target,
+                recursive,
+                keep_file,
+                stdout,
+            }
         })();
     }
 
@@ -31,8 +44,24 @@ fn parse_args<'a>() -> ArgMatches<'a> {
                 .required(true)
                 .index(1),
         )
-        // .arg(Arg::with_name("recursive").short("r").long("recursive").help("Processing files and directories recursively"))
-        // .arg(Arg::with_name("keep").short("k").long("keep").help("Keep original file"))
+        .arg(
+            Arg::with_name("recursive")
+                .short("r")
+                .long("recursive")
+                .help("Processing files and directories recursively"),
+        )
+        .arg(
+            Arg::with_name("keep")
+                .short("k")
+                .long("keep")
+                .help("Keep original file"),
+        )
+        .arg(
+            Arg::with_name("stdout")
+                .short("c")
+                .long("stdout")
+                .help("Write to standard output and don't delete input files"),
+        )
         .after_help(
             format!(
                 "\
